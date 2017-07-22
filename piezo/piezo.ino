46b8c42015a5129caa4f5bc1da79a986b272e599
Shayne Kelly II <skshayne@gmail.com>
2017-07-21 22:29:22 -0700

    Change buzzer alarm to Bluetooth alarm command Clean up code a lot

--- piezo/piezo.ino
+++ piezo/piezo.ino
@@ -1,106 +1,82 @@
-// HC-06 passcode is 1234
+/**
+ * I/O pin definitions
+ */
+int btOutPin = 1;
+int buzzerOutPin = 2;
+int sensorOutPin = 3;
+int motorOutPin = 11;
 
-#include <SoftwareSerial.h>
-SoftwareSerial BT(8,9); 
-
-int sensoroutput = 3; // the analog pin connected to the sensor
-//int ledoutput = 5; // pin connected to LED
-int buzzeroutput = 2; // pin connected to buzzer
-//int vibePin = 5;
-int motorPin = 11;
+/**
+ * Constants
+ */
 int PIEZO_THRESHOLD = 255;
-int DELAY_MS = 5000;
-//int ledCount = 0;
+int DELAY_MS = 10000;
+int VIBRATE_MS = 3000;
 int vibrated = 0;
 
-int btOutPin = 1;
-
 void setup() {
-  // Setup I/O pins
-  pinMode(sensoroutput, OUTPUT);
-  pinMode(motorPin, OUTPUT);
-  pinMode(buzzeroutput, OUTPUT);
+  /* Setup I/O pins */
+  pinMode(sensorOutPin, OUTPUT);
+  pinMode(motorOutPin, OUTPUT);
+  pinMode(buzzerOutPin, OUTPUT);
   pinMode(btOutPin, OUTPUT);
 
-  // set baud rate
+  /** 
+   *  Setup bluetooth connection with HC-06.
+   *  NOTE: Pulling up the key pin forces the baud rate to 38400.
+   *  TODO: Change name of HC-06 module to SnoozeBud. 
+   *        Change passcode (default 1234).
+   */
   Serial.begin(38400);
-  Serial.print("AT");
-  delay(200);
-  Serial.print("AT+BAUD8");
-  delay(200);
   Serial.flush();
-  //Serial.begin(115200);
-  
-//  BT.begin(115200);
-//  delay(1000);
-//  BT.print("AT+BAUD4");
-//  Serial1.begin(9600);
-//  BT.print("AT+BAUD4");
-//  Serial1.print("BT init");
 }
 
-// Loop to read piezo vibration sensor
+/**
+ * Continuously check the piezo-electric sensor for movement.
+ * Vibrate/alarm happens after a 10 second lapse in breathing.
+ * Alarm occurs after 2 vibrations are issued.
+ */
 void loop() {
-  char c = 'c';
-//  Serial.println("serial Test");
-  Serial.println("ALARM");
-  delay(1000);
-  if (Serial.available()) {
-    char a = (Serial.read());
-    Serial.println(a);
+  /* If no movement detected, countdown for the specified delay (10s) waiting for movement */
+  if (!movementDetected()) {
+    unsigned long countdownInit = millis();
+    while ((millis() - countdownInit) < DELAY_MS) {
+      if (movementDetected()) {
+        /* Movement detected, break out of the countdown loop and restart checking */
+        return;
+      }
+    }
+
+    /* No movement detected in 10s, send alert (vibration or alarm) */
+    if (vibrated < 2) { /* Vibrate twice before sending alarm */
+      /* Vibrate for 3s, but stop immediately if movement detected */
+      analogWrite(motorPin, 255);
+      unsigned long countdown = millis();
+      while ((millis() - countdown) < VIBRATE_MS) {
+        if (movementDetected()) {
+          /* Break out of countdown loop to resume movement checking, turn off motor */
+          analogWrite(motorPin, 0);
+          return;
+        }
+      }
+      /* No movement detected during vibration, turn off motor and increment vibration counter */
+      analogWrite(motorPin, 0);
+      vibrated++;
+    } else {
+      /* Already 2 vibrations sent, send Bluetooth alarm command to phone */
+      sendAlarmCommand();
+      vibrated = 0;
+    }
   }
-//  digitalWrite(buzzeroutput, LOW);
-//  if (movementDetected()) {
-//    analogWrite(motorPin, 0);
-//  } else {
-//    // Countdown for the specified delay (10s) while waiting for movement
-//    unsigned long countdownInit = millis();
-//    while ((millis() - countdownInit) < DELAY_MS) {
-//      if (movementDetected()) {
-//        // Break out of countdown/loop and turn off LED
-//        analogWrite(motorPin, 0);
-//        return;
-//      }
-//    }
-//
-//    // Send vibration
-//    if (vibrated < 2) {
-//      analogWrite(motorPin, 255);
-//      unsigned long countdown = millis();
-//      while ((millis() - countdown) < DELAY_MS) {
-//        if (movementDetected()) {
-//          // Break out of countdown/loop and turn off LED
-//          analogWrite(motorPin, 0);
-//          digitalWrite(buzzeroutput, LOW);
-//          return;
-//        }
-//      }
-//      analogWrite(motorPin, 0);
-//      vibrated++;
-//    } else {
-//      turnOnBuzzerIfNeeded();
-//      vibrated = 0;
-//    }
-//  }
 }
 
-// Checks piezo analog input voltage reading for movement
+/* Checks piezo analog input voltage reading for movement */
 boolean movementDetected() {
   return (analogRead(sensoroutput) >= PIEZO_THRESHOLD);
 }
 
-// Buzzer will turn on after LED turns on twice
-void turnOnBuzzerIfNeeded() {
-    digitalWrite(buzzeroutput, HIGH);
-    delay(3000);
-//    unsigned long countdownInit = millis();
-//    while ((millis() - countdownInit) < 3000) {
-//      if (movementDetected()) {
-//        digitalWrite(buzzeroutput, LOW);
-//        return;
-//      }
-    digitalWrite(buzzeroutput, LOW);
-    return;
-//  }
+/* Send alarm command via Bluetooth to the paired phone */
+void sendAlarmCommand() {
+    Serial.println("ALARM:ON");
 }
 
